849c18d47699e86a266c83ab4e0ba4a2f56adc50
Nek <nek-@live.fr>
2013-12-01 13:09:49 +0100

    Started to implement angular on the front

--- assets/linker/js/main.js
+++ assets/linker/js/main.js
@@ -1,11 +1,10 @@
-/**
- * app.js
- *
- * This file contains some conventional defaults for working with Socket.io + Sails.
- * It is designed to get you up and running fast, but is by no means anything special.
- *
- * Feel free to change none, some, or ALL of this file to fit your needs!
- */
+'use strict';
+
+var akemichatApp = angular.module('akemichatApp', [
+    'ngRoute',
+    'akemichatControllers',
+    'btford.socket-io'
+]);
 
 
 (function (io) {
